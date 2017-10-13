d3520cb540b11bd54dab9fe1b99a87afbe7000d1
Camille VIVIER <camille.vivier@hotmail.fr>
2017-10-13 17:20:30 +0200

    API swapi Update third and fourth request with all characters from all pages

--- js/main.js
+++ js/main.js
@@ -17,19 +17,25 @@ $(document).ready(function() {
 
 
 /* Afficher la liste des personnages et générer leurs modales correspondantes */
-	var swapi3 = "https://swapi.co/api/people/?format=json";
-	$.getJSON(swapi3, function(json) {
-		console.log(json.results);
-		var i = 1;
-		$.each(json.results, function(){
-			$('#list-characters').append('<h6><a data-toggle="modal" data-target="#modal-charac-' + i + '">' + this.name + '</a></h6>');
-			$('div#details-characters').append('<div id="modal-charac-' + i + '" class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">' + this.name + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul> </ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>')
-			for(var key in this) {
-			    $(".modal-body ul").append("<li>" + key + ' : ' + this[key] + "</li>")
-			}
-			i = i+1;
-		});
- 	});
+
+	for (i=1; i<=10; i++) {
+		// var swapi3 = "https://swapi.co/api/people/?format=json";
+		var swapi3 = "https://swapi.co/api/people/?format=json&page=" + i ;
+		$.getJSON(swapi3, function(json) {
+			console.log(json);
+			console.log(json.results);
+			var id = 1;
+			$.each(json.results, function(){
+				$('ol#list-characters').append('<li><a data-toggle="modal" data-target="#modal-charac-' + id + '">' + this.name + '</a></li>');
+				$('div#details-characters').append('<div id="modal-charac-' + id + '" class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">' + this.name + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul> </ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>')
+				for(var key in this) {
+				    $("#modal-charac-" + id + " .modal-body ul").append("<li>" + key + ' : ' + this[key] + "</li>")
+				}
+				id = id+1;
+			});
+	 	});
+		i = i+1;
+	};
 
 
 /* Faire en sorte d’afficher dans une modal le détails d’un personnage du clic sur celui ci. */
