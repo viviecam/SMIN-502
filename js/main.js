6ca875d1792c568a82d3b020bf0fcf9489aec661
Camille VIVIER <camille.vivier@hotmail.fr>
2017-10-18 12:12:48 +0200

    Add dynamic pagination

--- js/main.js
+++ js/main.js
@@ -34,23 +34,52 @@ $(document).ready(function() {
 
 
 /* Afficher la liste des personnages et générer leurs modales correspondantes */
-	
+
 	var id = 1;
-	var i=0;
-	for (i=1; i<=9; i++) {
-		var swapi3 = "https://swapi.co/api/people/?format=json&page=" + i ;
+	// for (i=1; i<=9; i++) {
+		var swapi3 = "https://swapi.co/api/people/?format=json&page=1" ;
 		$.getJSON(swapi3, function(json) {
 			// console.log(json.results);
 			$.each(json.results, function(){
-				$('ol#list-characters').append('<li><a data-toggle="modal" data-target="#modal-charac-' + id + '">' + this.name + '</a></li>');
+				$('ol#list-characters').append('<li class="col-12 col-md-4"><a data-toggle="modal" data-target="#modal-charac-' + id + '">' + this.name + '</a></li>');
 				$('div#details-characters').append('<div id="modal-charac-' + id + '" class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">' + this.name + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul> </ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>')
 				for(var key in this) {
 				    $("#modal-charac-" + id + " .modal-body ul").append("<li>" + key + ' : ' + this[key] + "</li>")
 				}
-				id = id+1;
+				id = id+1;	
 			});
+			var pre = json.previous;
+			var next = json.next;
+ 			console.log(pre, next);
+
+
+	 		$('#prevbutton').on('click', function(event){
+	 			event.preventDefault();
+	 			swapi3 = pre;
+	 			console.log(swapi3);
+
+	 			//if null then disabled button
+	 		});	 	
 	 	});
-	};
+	 	
+
+	// };
+	
+	// var id = 1;
+	// for (i=1; i<=9; i++) {
+	// 	var swapi3 = "https://swapi.co/api/people/?format=json&page=" + i ;
+	// 	$.getJSON(swapi3, function(json) {
+	// 		// console.log(json.results);
+	// 		$.each(json.results, function(){
+	// 			$('ol#list-characters').append('<li class="col-12 col-md-4"><a data-toggle="modal" data-target="#modal-charac-' + id + '">' + this.name + '</a></li>');
+	// 			$('div#details-characters').append('<div id="modal-charac-' + id + '" class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">' + this.name + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul> </ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>')
+	// 			for(var key in this) {
+	// 			    $("#modal-charac-" + id + " .modal-body ul").append("<li>" + key + ' : ' + this[key] + "</li>")
+	// 			}
+	// 			id = id+1;
+	// 		});
+	//  	});
+	// };
 
 
 /* Faire en sorte d’afficher dans une modal le détails d’un personnage du clic sur celui ci. */
