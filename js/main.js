d5de0575da4d2c040fadc7d6aea050adf8b90a74
Camille VIVIER <camille.vivier@hotmail.fr>
2017-10-18 08:48:37 +0200

    API SWAPI Update all characters all pages

--- js/main.js
+++ js/main.js
@@ -17,14 +17,13 @@ $(document).ready(function() {
 
 
 /* Afficher la liste des personnages et générer leurs modales correspondantes */
-
-	for (i=1; i<=10; i++) {
-		// var swapi3 = "https://swapi.co/api/people/?format=json";
+	
+	var id = 1;
+	var i=0;
+	for (i=1; i<=9; i++) {
 		var swapi3 = "https://swapi.co/api/people/?format=json&page=" + i ;
 		$.getJSON(swapi3, function(json) {
-			console.log(json);
-			console.log(json.results);
-			var id = 1;
+			// console.log(json.results);
 			$.each(json.results, function(){
 				$('ol#list-characters').append('<li><a data-toggle="modal" data-target="#modal-charac-' + id + '">' + this.name + '</a></li>');
 				$('div#details-characters').append('<div id="modal-charac-' + id + '" class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">' + this.name + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul> </ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>')
@@ -34,7 +33,6 @@ $(document).ready(function() {
 				id = id+1;
 			});
 	 	});
-		i = i+1;
 	};
 
 
