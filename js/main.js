589a285a2a5c3310521cca60ed22a3e279f9e9e3
Camille Vivier <camille.vivier@hotmail.fr>
2017-10-18 15:42:24 +0200

    Update dynamic pagination

--- js/main.js
+++ js/main.js
@@ -26,41 +26,48 @@ $(document).ready(function() {
 
 
 /* Afficher dans la console et dans le h2 le “Name” retourné.*/
-	var swapi1 = "https://swapi.co/api/people/1/?format=json";
-	$.getJSON(swapi1, function(json) {
-		console.log(json.name);
-		$('h2#name').html(json.name);
-	});
+	// var swapi1 = "https://swapi.co/api/people/1/?format=json";
+	// $.getJSON(swapi1, function(json) {
+	// 	console.log(json.name);
+	// 	$('h2#name').html(json.name);
+	// });
 
 
 /* Afficher la liste des personnages et générer leurs modales correspondantes */
+	var swapi = "https://swapi.co/api/people/?format=json&page=1" ;
 
+
+
+	getDonnees(swapi);
+});
+
+function getDonnees(swapi) {
+		
+		
 	var id = 1;
 	// for (i=1; i<=9; i++) {
-		var swapi3 = "https://swapi.co/api/people/?format=json&page=1" ;
-		$.getJSON(swapi3, function(json) {
-			// console.log(json.results);
-			$.each(json.results, function(){
-				$('ol#list-characters').append('<li class="col-12 col-md-4"><a data-toggle="modal" data-target="#modal-charac-' + id + '">' + this.name + '</a></li>');
-				$('div#details-characters').append('<div id="modal-charac-' + id + '" class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">' + this.name + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul> </ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>')
-				for(var key in this) {
-				    $("#modal-charac-" + id + " .modal-body ul").append("<li>" + key + ' : ' + this[key] + "</li>")
-				}
-				id = id+1;	
-			});
-			var pre = json.previous;
-			var next = json.next;
- 			console.log(pre, next);
-
-
-	 		$('#prevbutton').on('click', function(event){
-	 			event.preventDefault();
-	 			swapi3 = pre;
-	 			console.log(swapi3);
-
-	 			//if null then disabled button
-	 		});	 	
-	 	});
+	$.getJSON(swapi, function(json) {
+		console.log(json.next);
+		// console.log(json.results);
+		// $.each(json.results, function(){
+		// 	$('ol#list-characters').append('<li class="col-12 col-md-4"><a data-toggle="modal" data-target="#modal-charac-' + id + '">' + this.name + '</a></li>');
+		// 	$('div#details-characters').append('<div id="modal-charac-' + id + '" class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">' + this.name + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul> </ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>')
+		// 	for(var key in this) {
+		// 	    $("#modal-charac-" + id + " .modal-body ul").append("<li>" + key + ' : ' + this[key] + "</li>")
+		// 	}
+		// 	id = id+1;	
+		// });
+ 	
+ 	// 	$('#nextbutton').on('click', function(event){
+ 	// 		// $('ol#list-characters, div#details-characters').empty();
+ 	// 		swapi2 = json.next;
+ 			
+ 	// 		getDonnees(swapi2);
+ 	// 	});
+ 		$('#nextbutton').attr("onclick","getDonnees('"+json.next+"')");
+ 	});
+}
+	
 	 	
 
 	// };
@@ -69,7 +76,7 @@ $(document).ready(function() {
 	// for (i=1; i<=9; i++) {
 	// 	var swapi3 = "https://swapi.co/api/people/?format=json&page=" + i ;
 	// 	$.getJSON(swapi3, function(json) {
-	// 		// console.log(json.results);
+	// 		console.log(json.next);
 	// 		$.each(json.results, function(){
 	// 			$('ol#list-characters').append('<li class="col-12 col-md-4"><a data-toggle="modal" data-target="#modal-charac-' + id + '">' + this.name + '</a></li>');
 	// 			$('div#details-characters').append('<div id="modal-charac-' + id + '" class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">' + this.name + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul> </ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>')
@@ -112,4 +119,3 @@ $(document).ready(function() {
 	// 	$('.card-text').html(json.count);
  //        console.log(json.count);
  //    });
-});
